5248e1a709ce6ce53432a7c1ea3b038d37002a4d
emmy1999 <60434319+emmy1999@users.noreply.github.com>
2021-05-14 01:16:23 +0100

    remove file

--- firebaseauth/fauthcode/android/app/src/main/kotlin/com/example/fauthcode/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fauthcode
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- firebaseauth/fauthcode/lib/src/repository/my_user_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-import 'package:flutter_simple_firebase_auth/src/model/user.dart';
-
-abstract class MyUserRepositoryBase {
-  Future<MyUser?> getMyUser();
-
-  Future<void> saveMyUser(MyUser user, File? image);
-}
